ef6f9af3aeb3072ef15a4c042fc82165db25759e
Nathan Pickard <nathan.pickard87@gmail.com>
2016-09-01 21:07:23 -0700

    Added blog template

--- scripts/article.js
+++ scripts/article.js
@@ -9,7 +9,7 @@ function Article (opts) {
   this.body = opts.body;
 }
 
-Article.prototype.toHtml = funciton() {
+Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
 
   $newArticle.attr('data-category', this.category);
@@ -22,4 +22,27 @@ Article.prototype.toHtml = funciton() {
 
   // Include the publication date as a 'title' attribute to show on hover:
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-}
+
+  // Display the date as a relative number of "days ago":
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+
+  $newArticle.append('<hr>');
+
+  $newArticle.removeClass('template');
+
+  $newArticle.removeClass("template");
+
+  return $newArticle;
+};
+
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
+
+articles.forEach(function(a) {
+  $('#articles').append(a.toHtml());
+});
---
+++ scripts/blogArticles.js
@@ -0,0 +1,10 @@
+var rawData = [
+  {
+    title:        'First Blog Post!',
+    category:     'class-01',
+    author:       'Nate Pickard',
+    authorUrl:    'https://nathanpickard.squarespace.com',
+    publishedOn:  '2016-08-30',
+    body:         '<p>This is my first blog post for personal assignment</p>'
+  },
+];
