857b3158383663c6c63dfc1e88e96cd7c4b5e19a
Nathan Pickard <nathan.pickard87@gmail.com>
2016-09-05 21:57:04 -0700

    Added filters and reveals only specified section

---
+++ scripts/projectArticles.js
@@ -0,0 +1,10 @@
+var rawData = [
+  {
+    title:        'First project!',
+    category:     'class-01',
+    author:       'Nathan Pickard',
+    authorUrl:    'https://nathanpickard.squarespace.com',
+    publishedOn:  '2016-08-30',
+    body:         '<p>This is my first project for personal assignment</p>'
+  },
+];
