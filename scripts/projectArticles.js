fb1af77c29b12757dc3e52d59dc0225d04e2fa0b
Nathan Pickard <nathan.pickard87@gmail.com>
2016-09-05 22:04:59 -0700

    Added second project placeholder

--- scripts/projectArticles.js
+++ scripts/projectArticles.js
@@ -7,4 +7,12 @@ var rawData = [
     publishedOn:  '2016-08-30',
     body:         '<p>This is my first project for personal assignment</p>'
   },
+  {
+    title:        'Second project goes here!',
+    category:     'class-02',
+    author:       'Nathan Pickard',
+    authorUrl:    'https://nathanpickard.squarespace.com',
+    publishedOn:  '2016-09-05',
+    body:         '<p>My second project for my portfolio will go here</p>'
+  }
 ];
