b5075e65e9671202bc706b2763bf5c23a0a8ec2f
Nathan Pickard <nathan.pickard87@gmail.com>
2016-09-20 21:45:02 -0700

    Adjusted the Read on and the project publication dates

--- scripts/projectArticles.js
+++ scripts/projectArticles.js
@@ -4,7 +4,7 @@ var rawData = [
     category:     'class-01',
     project:      'Nathan Pickard',
     authorUrl:    'https://nathanpickard.squarespace.com',
-    publishedOn:  '2016-08-30',
+    publishedOn:  '08-30-2016',
     body:         '<p>This is my first project for personal assignment</p>'
   },
   {
@@ -12,7 +12,7 @@ var rawData = [
     category:     'class-02',
     project:      'Nathan Pickard',
     authorUrl:    'https://nathanpickard.squarespace.com',
-    publishedOn:  '2016-09-05',
+    publishedOn:  '09-05-2016',
     body:         '<p>My second project for my portfolio will go here</p>'
   }
 ];
