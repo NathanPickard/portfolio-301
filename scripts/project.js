e2bb4dc6735adee2abd3b2595b6474770ecb8618
Nathan Pickard <nathan.pickard87@gmail.com>
2016-10-06 17:49:52 -0700

    Added IIFE to project.js and .map function

--- scripts/project.js
+++ scripts/project.js
@@ -1,4 +1,4 @@
-// var projects = [];
+(function(module) {
 
 function Project (opts) {
   this.title = opts.title;
@@ -27,54 +27,68 @@ Project.loadAll = function(rawData) {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
   });
 
-  rawData.forEach(function(ele) {
-    Project.all.push(new Project(ele));
+  // rawData.forEach(function(ele) {
+  //   Project.all.push(new Project(ele));
+  // }
+  Project.all = rawData.map(function(ele) {
+    return new Project(ele);
   });
-}
+};
 
-
-Project.fetchAll = function() {
-  $.ajax({
-    url: './data/projectArticles.json',
-    method: 'HEAD',
-    success: function(data, message, xhr) {
-      console.log('xhr', xhr);
-      var etag = xhr.getResponseHeader('ETag');
-      console.log('etag', etag);
-      if(localStorage.etag) {
-        var locEtag = localStorage.getItem('etag');
-        if (locEtag === etag && localStorage.rawData) {
-          console.log('etag matches and in local storage');
-          fetchFromLocalStorage();
-        } else {
-          fetchFromDisk();
-        }
-      }  else {
-          fetchFromDisk();
-        }
-        localStorage.setItem('etag', etag);
-      }
-  });
-
-  function fetchFromDisk() {
-    console.log('using ajax');
-    $.getJSON('./data/projectArticles.json', function(data) {
-      Project.loadAll(data);
-      localStorage.setItem('rawData', JSON.stringify(data));
-      projectView.initIndexPage();
+Project.fetchAll = function(callback) {
+  if (localStorage.rawData) {
+    Project.loadAll(JSON.parse(localStorage.rawData));
+    callback();
+  } else {
+    $.getJSON('./data/projectArticles.json', function(rawData) {
+      Project.loadAll(rawData);
+      localStorage.rawData = JSON.stringify(rawData);
+      callback();
     });
   }
-
-  function fetchFromLocalStorage() {
-    console.log('using local storage');
-    var rd = localStorage.getItem('rawData');
-    var rdjson = JSON.parse(rd);
-    Project.loadAll(rdjson);
-    projectView.initIndexPage();
-  }
-}
+};
 
 
-// projects.forEach(function(a) {
-//   $('#projects').append(a.toHtml());
-// });
+// Project.fetchAll = function() {
+//   $.ajax({
+//     url: './data/projectArticles.json',
+//     method: 'HEAD',
+//     success: function(data, message, xhr) {
+//       console.log('xhr', xhr);
+//       var etag = xhr.getResponseHeader('ETag');
+//       console.log('etag', etag);
+//       if(localStorage.etag) {
+//         var locEtag = localStorage.getItem('etag');
+//         if (locEtag === etag && localStorage.rawData) {
+//           console.log('etag matches and in local storage');
+//           fetchFromLocalStorage();
+//         } else {
+//           fetchFromDisk();
+//         }
+//       }  else {
+//           fetchFromDisk();
+//         }
+//         localStorage.setItem('etag', etag);
+//       }
+//   });
+//
+//   function fetchFromDisk() {
+//     console.log('using ajax');
+//     $.getJSON('./data/projectArticles.json', function(data) {
+//       Project.loadAll(data);
+//       localStorage.setItem('rawData', JSON.stringify(data));
+//       projectView.initIndexPage();
+//     });
+//   }
+//
+//   function fetchFromLocalStorage() {
+//     console.log('using local storage');
+//     var rd = localStorage.getItem('rawData');
+//     var rdjson = JSON.parse(rd);
+//     Project.loadAll(rdjson);
+//     projectView.initIndexPage();
+//   }
+// }
+
+  module.Project = Project;
+})(window);
